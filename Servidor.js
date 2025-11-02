922d8d5de8705625e0965d73f2fe5ebc003cb992
Benjamin <bzenteno@it.ucsc.cl>
2025-11-01 22:53:54 -0300

    Creacion de paginas de admin y entradas de palco, galeria y platea

--- Servidor.js
+++ Servidor.js
@@ -150,6 +150,55 @@ app.delete('/eliminarPL/:id',(req,res)=>{
         res.status(404).json({error:'Hubo un error al eliminar la entrada'});
     }
 });  
+async function generarHTML(nombre, telefono, correo, direccion, imagenUrl) {
+  return `
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Contacto</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="inicio.css">
+    </head>
+    <header>
+        <nav class="navbar barranav">
+            <div class="container-fluid barranav">
+                <a href="#">
+                    <img src="Contenido/Logo-DPP-300dpi-4.png" alt="Logo" width="250" height="80">
+                </a>
+            </div>
+        </nav>
+    </header>
+    <body>
+        <br>
+        <div class="bs-primary-border-subtle container">
+            <div class="marcofoto">
+                <div class="foto">
+                    ${imagenUrl ? `<img src="${imagenUrl}">` : ''}
+                </div>
+            </div>
+            <div class="info">
+                <h1 class="titulo">Datos de contacto</h1>
+                <div class="letras">
+                    <h4 class="nombre-defensor">${nombre}</h4>
+                    <h4>Teléfono: ${telefono}</h4>
+                    <h4>Email: ${correo}</h4>
+                    <h4>Dirección: ${direccion}</h4>
+                    <br><br>
+                    <a href="#">
+                        <button class="boton">AGENDAR CITA</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </body>
+    <footer></footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+    </html>
+  `;
+}
+
 app.listen(port,()=>{
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}/admin.html`);
 }   );
